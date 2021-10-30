9d2a7030c816fc982a80acb8a5af9000d586744d
WingarWang <wyjj@bu.edu>
2021-10-29 21:50:05 -0400

    final data

--- final_datawrangling.R
+++ final_datawrangling.R
@@ -130,30 +130,39 @@ distinct(strawb_other, details)
 distinct(strawb_na, details)
 
 #############################################################
-#############################################################
 
 pesticides <- read_csv("Pesticides(1).csv")
 pesticides
 
-strawb_select <- select(strawb, Year, State, items, discription, units, dname, type, details, Value)
-strawb_select
+#Delete "N/A" from "Pesticide"
 
-#去除"Pesticide"和"details"的NA项
 pesticides <- filter(pesticides, !is.na(Pesticide))
-strawb_select <- filter(strawb_select, !is.na(details))
 
-strawb_select %<>% separate(col = details, 
+#Delete "N/A" from "details"
+
+strawb_new <- filter(strawb, !is.na(details))
+
+strawb_new %<>% separate(col = details, 
                             into = c("chemical_name", "chemical_id"), 
                             sep = "=", 
                             fill = "right")
 
 #使"Pesticide"变成大写，这样可以match数据集strawb里的大写
-pesticides <- mutate(pesticides, Pesticide = toupper(Pesticide))
-#trimws：删除前导/尾随空格
-strawb_select <- mutate(strawb_select, chemical_name = trimws(chemical_name))
 
-joined <- inner_join(strawb_select, pesticides, 
+pesticides <- mutate(pesticides, Pesticide = toupper(Pesticide))
+
+#trimws：删除前导/尾随空格
+strawb_new <- mutate(strawb_new, chemical_name = trimws(chemical_name))
+
+## join two files
+
+joined <- inner_join(strawb_new, pesticides, 
                      by = c("chemical_name" = "Pesticide"))
+
+distinct(strawb_new, type)
+distinct(strawb_new,discription)
+
+joined_new<-select(joined,Year,State,discription,type,chemical_name,Value, Carcinogen,"Hormone Disruptor",Neurotoxins,"Developmental or Reproductive Toxins","Bee Toxins")
 
 #############################################################
 
